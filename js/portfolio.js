b4dca58aac90d9dc546af879ec57514e8c2328bb
Chris Mayer <mayerxc@gmail.com>
2017-10-31 17:04:54 -0400

    updating portfolio with bootstrap 4

---
+++ js/portfolio.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    console.log('script loaded');
+    $(function () {
+        $('nav a').on('click', function () {
+            console.log('.nav a click triggered');
+            if ($('.navbar-toggler').css('display') != 'none') {
+                $(".navbar-toggler").trigger("click");
+                console.log("click was triggered");
+            }
+        });
+    });
+
+
+
+    document.getElementById("testButton").addEventListener("click", function () {
+        $("#tester").html("button was pressed!");
+    });
+
+});
